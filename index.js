f368ef089efb53d7422f9340239c73d47db86196
Brandon Davis <brandon.davis@conehealth.com>
2022-08-02 08:43:46 -0400

    setup inquirer utility and an returning data

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Employee = require('./lib/Employee');
+const prompt = require('./utils/prompts');
 
 
 const employee = new Employee('Brandon', '1234', 'test@email.com');
 
 console.log(employee.getEmail());
+
+const answers = prompt().then(result => console.log(result));
+
+
+
--- utils/prompts.js
+++ utils/prompts.js
@@ -1,1 +1,16 @@
 const inquirer = require('inquirer');
+
+
+const managerPrompt = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                question: 'What is your name?'
+            },            
+        ])
+    .then(employeeData => employeeData)
+}
+
+module.exports = managerPrompt;
