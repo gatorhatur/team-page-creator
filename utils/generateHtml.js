81fbd94ec381e031588d4f7779f7fd56702e542b
Brandon Davis <brandon.davis@conehealth.com>
2022-08-04 19:01:47 -0400

    created sample html and started on html generation functions

---
+++ utils/generateHtml.js
@@ -0,0 +1,80 @@
+function getIcon(role) {
+    retu
+}
+
+function generateCard(employeeObj) {
+    return `
+    <div class="card mt-10 shadow" style="width: 18rem;">
+    <div class="card-img-top bg-warning">
+        <h3 class="p-10">Name</h3>
+        <h5 class="p-10"><span id="icon"></span>Manager</h5>
+    </div>
+    <div class="card-body">
+      <p class="card-text">ID: </p>
+      <p class="card-text">Email: </p>
+      <p class="card-text">Office Number: </p>
+    </div>
+    </div>
+    `
+}
+
+function generateHtml(employeeArr) {
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha512-UyNhw5RNpQaCai2EdC+Js0QL4RlVmiq41DkmCJsRV3ZxipG2L0HhTqIf/H9Hp8ez2EnFlkBnjRGJU2stW3Lj+w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!--Will need to be updated to best practice path-->
+    <link rel="stylesheet" href="./assets/css/style.css" />
+
+    <title>Get to know your Team!</title>
+</head>
+<body>
+    <header class="container-fluid bg-info h-25">
+        <h1 class="text-light text-center">Meet our Team!</h1>
+    </header>
+
+    <main class="container mt-10 d-flex flex-wrap">
+
+        ${generateCards()}
+
+
+
+          <div class="card mt-10 shadow" style="width: 18rem;">
+            <div class="card-img-top bg-warning">
+                <h3 class="p-10">Name</h3>
+                <h5 class="p-10"><span id="icon"></span>Engineer</h5>
+            </div>
+            <div class="card-body">
+                <p class="card-text">ID: </p>
+                <p class="card-text">Email: </p>
+                <p class="card-text">Github: </p>
+            </div>
+          </div>
+
+          <div class="card mt-10 shadow" style="width: 18rem;">
+            <div class="card-img-top bg-warning">
+                <h3 class="p-10">Name</h3>
+                <h5 class="p-10"><span id="icon"></span>Intern</h5>
+            </div>
+            <div class="card-body">
+                <p class="card-text">ID: </p>
+                <p class="card-text">Email: </p>
+                <p class="card-text">School: </p>
+            </div>
+          </div>
+          
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+</body>
+</html>    
+    `
+}
